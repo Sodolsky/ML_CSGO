bd972903aaf0adab6256e2cd6c4aa634ef39fa6d
Sodolsky <mateuszsodolskiofficial@gmail.com>
2023-06-22 12:38:46 +0200

    Changed comments co Polish

--- MachineLearningScript.py
+++ MachineLearningScript.py
@@ -1,3 +1,4 @@
+# Importowanie niezbędnych bibliotek
 import matplotlib.pyplot as plt
 import pandas as pd
 import requests
@@ -8,13 +9,13 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from tensorflow import keras
 
-# Download dataset
+# Pobranie zbioru danych
 url = "https://www.openml.org/data/download/22102255/dataset"
 r = requests.get(url, allow_redirects=True)
 with open("dataset.txt", "wb") as f:
     f.write(r.content)
 
-# Read data from dataset file
+# Odczytanie danych z pliku zbioru
 data = []
 with open("dataset.txt", "r") as f:
     for line in f.read().split("\n"):
@@ -22,26 +23,26 @@ with open("dataset.txt", "r") as f:
             continue
         data.append(line)
 
-# Extract column names
+# Wyodrębnienie nazw kolumn
 columns = []
 with open("dataset.txt", "r") as f:
     for line in f.read().split("\n"):
         if line.startswith("@ATTRIBUTE"):
             columns.append(line.split(" ")[1])
 
-# Save data to CSV file
+# Zapisanie danych do pliku CSV
 with open("df.csv", "w") as f:
     f.write(",".join(columns))
     f.write("\n")
     f.write("\n".join(data))
 
-# Read data from CSV into a DataFrame
+# Odczytanie danych z pliku CSV do DataFrame
 df = pd.read_csv("df.csv")
 
-# Encode the target variable
+# Zakodowanie zmiennej docelowej
 df['t_win'] = df.round_winner.astype("category").cat.codes
 
-# Calculate correlations and select columns that have the highest correlation
+# Obliczenie korelacji i wybór kolumn o najwyższej korelacji
 correlations = df[columns+["t_win"]].corr(numeric_only=True)
 selected_columns = []
 for col in columns+["t_win"]:
@@ -52,21 +53,21 @@ for col in columns+["t_win"]:
         pass
 df_selected = df[selected_columns]
 
-# Plot correlation heatmap
+# Wygenerowanie wykresu korelacji
 plt.figure(figsize=(18, 12))
 sns.heatmap(df_selected.corr().sort_values(by="t_win"), annot=True, cmap="YlGnBu")
-plt.savefig("Correlation_Map.png")
-# Split data into training and testing sets
+plt.savefig("Mapa_Korelacji.png")
+
+# Podział danych na zbiór treningowy i testowy
 X, y = df_selected.drop(["t_win"], axis=1), df_selected["t_win"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Scale the feature data
+# Standaryzacja danych cech
 scaler = StandardScaler()
 XTrainScaled = scaler.fit_transform(X_train)
 XTestScaled = scaler.transform(X_test)
 
-
-# Create a neural network model
+# Utworzenie modelu sieci neuronowej
 model = keras.models.Sequential()
 model.add(keras.layers.Input(shape=(20,)))
 model.add(keras.layers.Dense(200, activation="relu"))
@@ -74,12 +75,16 @@ model.add(keras.layers.Dense(100, activation="relu"))
 model.add(keras.layers.Dense(50, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
 
-# Compile the model
+# Kompilacja modelu
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-# Train the model with early stopping
+# Trening modelu z wcześniejszym zatrzymaniem
 early_stopping_callback = keras.callbacks.EarlyStopping(patience=5)
 XTrainScaledTrain, XValidation, y_train_train, y_valid = train_test_split(XTrainScaled, y_train, test_size=0.15)
-model.fit(XTrainScaledTrain,y_train_train,epochs=30,callbacks=[early_stopping_callback],validation_data=(XValidation,y_valid))
-model.evaluate(XTestScaled,y_test)
+model.fit(XTrainScaledTrain, y_train_train, epochs=30, callbacks=[early_stopping_callback], validation_data=(XValidation, y_valid))
+
+# Ocena modelu na zbiorze testowym
+model.evaluate(XTestScaled, y_test)
+
+# Zapisanie modelu
 model.save("Round_Predictions_CSGO.h5")
